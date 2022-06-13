7b1dec0c100696cec8ab064ffabfb525f7c134f2
Esteban Carvajal <johan.gal.car@gmail.com>
2022-06-13 11:55:34 -0500

    Project finished

--- src/components/FormGoogleLogIn.tsx
+++ src/components/FormGoogleLogIn.tsx
@@ -18,38 +18,26 @@ const GoogleLogIn: React.FunctionComponent = () => {
 
     signInWithPopup(auth, providerGoogleAuth)
     .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
+      
       const credential:OAuthCredential | null = GoogleAuthProvider.credentialFromResult(result);
 
       const token = credential!.accessToken;
 
-      // The signed-in user info.
-      //If the logged in is succesfull you will acces this part of the code where you will 
-      //get a lot of information about the user that have logged in
       const user = result.user;
-
-      /*Whit the information of the user you can populate an state that is mainly focused on 
-        holding the information of the user that is logged in*/
 
       dispatch(logInInReducer(user))
       
       navigate('/hardwarestore/provider')
 
-      // ...
     }).catch((error) => {
 
-      //If the logged in is not succesfull yu will get to this part and with the message you can tell 
-      //the user what went wrong
-
-
-      // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
+      
       const email = error.email;
-      // The AuthCredential type that was used.
+      
       const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+      
     });
   }
 
